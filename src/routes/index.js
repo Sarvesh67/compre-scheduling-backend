3c50ea09ac49bd8df38d58bb2aee7ff2391d1d0b
Arjun Bajpai <arjunbajpaiwork@gmail.com>
2021-07-11 02:29:13 +0530

    added exam room endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const userRoutes = require('./user.route');
 const examRoutes = require('./exam.route');
 const roomRoutes = require('./room.routes');
+const examRoomRoutes = require('./examRoom.routes');
 
 const router = express.Router();
 
 router.use('/user', userRoutes);
 router.use('/exam', examRoutes);
 router.use('/room', roomRoutes);
+router.use('/examroom', examRoomRoutes);
 
 module.exports = router;
