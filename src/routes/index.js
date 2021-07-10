d1031cc86230c0d0df3ef12d44ec949d757fd357
Samesh Lakhotia <samesh.lakhotia@gmail.com>
2021-07-10 22:10:35 +0530

    Add Course and Invigilator table APIS

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const userRoutes = require('./user.route');
 const examRoutes = require('./exam.route');
+const invigilatorRoutes = require('./invigilator.route');
+const courseRoutes = require('./course.route');
+
 
 const router = express.Router();
 
 router.use('/user', userRoutes);
 router.use('/exam', examRoutes);
+router.use('/invigilator', invigilatorRoutes);
+router.use('/course', courseRoutes);
 
 module.exports = router;
