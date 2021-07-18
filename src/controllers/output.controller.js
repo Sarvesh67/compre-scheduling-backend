39c2925b0c3a4d10c0c4b23fe6bd16458696b87f
Kanchuking1 <kanchuking1@gmail.com>
2021-07-18 12:08:40 +0530

    Output 1 working

--- src/controllers/output.controller.js
+++ src/controllers/output.controller.js
@@ -7,6 +7,108 @@ const { Op } = require('sequelize')
 const sched = db.public.schedules
 const statics = db.public.statics
 
+const getOutput1 = async (req, res) => {
+    try {
+        var jsondata = []
+        scheduleId = req.params.schedId
+        /**'
+         * course - 
+         * title - 
+         * ic -
+         * date - 
+         * time - 
+         * Rooms -
+         * invigilators Required - 
+         * invgilators Given -
+         */
+
+        const exams = await sched.exam.findAll({
+            where:{
+                date:{
+                    [Op.not]: null
+                },
+                time: {
+                    [Op.not]: null
+                },
+                schedule_id: scheduleId
+            },
+            include: [
+                {
+                    model: sched.examRoom,
+                    include: [
+                        sched.invigilatorsAlloted,
+                        {
+                            model: statics.rooms,
+                            attributes: ['name']
+                        }
+                    ]
+                }
+            ]
+        })
+
+        for (const exam of exams){
+            //console.log(exam)
+            const course = await statics.courses.findOne({
+                where:{
+                    id: exam.course_id
+                },
+                include:[statics.invigilators]
+            })
+            const invigilators = course.invigilators;
+            var ic;
+            await invigilators.forEach(async (invigilator) => {
+                if(invigilator.courses_invigilators.status == 'ic'){
+                    ic = invigilator.name
+                }
+            })
+            var roomsString = ""
+            var invigilatorsGiven = 0
+            for (const room of exam.exam_rooms){
+                roomsString += room.room.name + "(" + room.capacity + "), "
+                invigilatorsGiven += room.invigilatorsAlloteds.length
+            }
+
+            const date = new Date(exam.date);
+            const dateString = date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear();
+            var time;
+            if(exam.time == '2-5'){
+                time = '2:00pm-5:00pm'
+            }
+            else{
+                time = '9:00am-12noon'
+            }
+            //console.log(roomsString)
+            newRow = {
+                Course : course.bits_id,
+                Title : course.title,
+                IC : ic,
+                Date : dateString,
+                Time : time,
+                Rooms : roomsString,
+                "Invigilators Required" : course.invigilators_required,
+                "Invigilators Given" : invigilatorsGiven
+            }
+
+            jsondata.push(newRow)
+        }
+
+        //console.log(jsondata)
+        const ws = XLSX.utils.json_to_sheet(jsondata);
+        const wb = XLSX.utils.book_new();
+	    XLSX.utils.book_append_sheet(wb, ws, 'Schedule');
+        XLSX.writeFile(wb , 'output1.xlsx');
+        var file = fs.readFileSync('output1.xlsx')
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        res.setHeader("Content-Disposition", "attachment; filename=" + "output1.xlsx");
+        res.end(file);
+    } catch (error) {
+        console.log(error);
+		return res.status(500).json({
+			msg: 'Internal server error :('
+		});
+    }
+}
+
 const getOutput2 = async (req,res) =>{
     try {
         var jsondata = [];
@@ -25,8 +127,6 @@ const getOutput2 = async (req,res) =>{
             }
         })
         for(const exam of exams){
-            //Get course details
-            //console.log(exam.dataValues);
             const course = await statics.courses.findOne({
                 where:{
                     id: exam.course_id
@@ -35,7 +135,6 @@ const getOutput2 = async (req,res) =>{
             })
             const invigilators = course.invigilators;
             var ic;
-            //console.log(course.dataValues);
             await invigilators.forEach(async (invigilator) => {
                 if(invigilator.courses_invigilators.status == 'ic'){
                     ic = invigilator.name
@@ -84,5 +183,6 @@ const getOutput2 = async (req,res) =>{
 }
 
 module.exports = {
-    getOutput2: getOutput2
+    getOutput1: getOutput1,
+    getOutput2: getOutput2,
 }
--- src/routes/output.route.js
+++ src/routes/output.route.js
@@ -3,6 +3,7 @@ const outputController = require('../controllers/output.controller')
 
 const router = express.Router()
 
+router.get('/one/:schedId', outputController.getOutput1);
 router.get('/two/:schedId', outputController.getOutput2);
 
 module.exports = router;
