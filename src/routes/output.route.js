3dc9aa8f9532dbbcb8db05d4fb962143ff8aebc3
Kanchuking1 <kanchuking1@gmail.com>
2021-08-06 13:34:28 +0530

    Bug Fixed

--- src/routes/output.route.js
+++ src/routes/output.route.js
@@ -7,5 +7,6 @@ router.get('/one/:schedId', outputController.getOutput1);
 router.get('/two/:schedId', outputController.getOutput2);
 router.get('/three/:bitsId', outputController.getOutput3);
 router.get('/four/:invigilatorId', outputController.getOutput4);
+router.get('/five/:schedId', outputController.getOutput5);
 
 module.exports = router;
